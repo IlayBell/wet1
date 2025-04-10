501d4cb786406fb00463c811adf9d7e730a1ab87
Ilay Bell <ilay.ben@campus.technion.ac.il>
2025-04-10 14:53:42 +0300

    still in progress

--- scrape_news.sh
+++ scrape_news.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Gets a list of all articles from web
-wget -q https://www.ynetnews.com/category/3082
+content=wget -q -O - "https://www.ynetnews.com/category/3082"
 articles=($(grep -o -E "https://www.ynetnews.com/article/[a-zA-Z0-9]*" ./3082 | sort | uniq))
 
 # Prints num of articles
@@ -20,7 +20,7 @@ for article in ${articles[@]}; do
 	
 	# Print result accordin to format
 	if [[ netanyahu -eq 0 ]] && [[ gantz -eq 0 ]]  && [[ bennett -eq 0 ]] && [[ peretz -eq 0 ]]; then
-		echo "$article, -"
+		echo "$article,-"
 	else
 		echo "$article, Netanyahu, $netanyahu, Gantz, $gantz, Bennett, $bennett, Peretz, $peretz"
 	fi
