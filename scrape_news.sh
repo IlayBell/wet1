923f607fbe0edae964833e92dcf4f4bbd21f1195
Ilay Bell <ilay.ben@campus.technion.ac.il>
2025-04-11 16:39:09 +0300

    yay!

--- scrape_news.sh
+++ scrape_news.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Gets a list of all articles from web
-content=wget -q -O - "https://www.ynetnews.com/category/3082"
-articles=($(grep -o -E "https://www.ynetnews.com/article/[a-zA-Z0-9]*" ./3082 | sort | uniq))
+content=$(wget -q -O - "https://www.ynetnews.com/category/3082")
+articles=($(echo $content | grep -o -E "https://www.ynetnews.com/article/[a-zA-Z0-9]*" | sort | uniq))
 
 # Prints num of articles
 echo ${#articles[@]}
